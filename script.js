7a5f582fa10afe5880652daf7711a1a58c9409f0
Sanjibpradhan380 <sanjibpradhan380@gmail.com>
2025-12-13 03:51:14 +0530

    love emogi animation first

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-let h1 = document.querySelector("h1");
-h1.style.color = "red";
+let img = document.querySelector(".card img");
+let love = document.querySelector("i");
+
+
+
+img.addEventListener('dblclick',function(){
+    love.style.opacity = 1;
+    love.style.transform = `translate(-50%,-50%) scale(1) rotate(0deg)`;
+    
+
+    setTimeout(function(){
+     love.style.transform = `translate(-50%,-300%) scale(1) rotate(60deg)`;
+    },800)
+    setTimeout(function(){
+        love.style.opacity = 0;
+    },1000)
+
+    setTimeout(function(){
+        llove.style.transform = `translate(-50%,-50%) scale(0) rotate(-60deg)`;
+    },1100)
+})
