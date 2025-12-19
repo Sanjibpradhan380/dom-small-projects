2bffdc5d332f54e81efa9e44f62abbe19a884f92
Sanjibpradhan380 <sanjibpradhan380@gmail.com>
2025-12-19 15:23:36 +0530

    how piano works

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 
-var main = document.querySelector("#main")
+let keys= document.querySelectorAll('.key');
 
-let cursor = document.querySelector('#cursor')
+const sounds ={
+    0:'28.mp3',
+    1:'29.mp3',
+    2:'30.mp3',
+    3:'31.mp3',
+    4:'32.mp3',
+    5:'33.mp3',
+    6:'34.mp3',
+    7:'35.mp3',
+    8:'36.mp3',
+    9:'37.mp3',
+    10:'38.mp3',
+    11:'39.mp3'
+};
 
-main.addEventListener('mousemove',function(dets){
-    cursor.style.left = dets.x+'px';
-    cursor.style.top = dets.y+'px';
-})
+keys.forEach((key, index) => {
+key.addEventListener('click', () => {
+const audio = new Audio(sounds[index]);
+audio.currentTime = 0;
+audio.play();
+});
+});
 
-let body = document.body
-let box = document.querySelector('#box')
-body.addEventListener('keydown',function(dets){
-    box.innerHTML =`${dets.code}`
-    
-})
+
+let bank =['manushya','rakhda','diksha','sonam'];
+bank.forEach((key,index)=>{
+    console.log(`${key}, ${index}`);
+});
